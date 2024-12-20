3acf9e0783faaed990b6f188e51ab0f7115184da
Tanmay Chaturvedi <tanmayreadsandwrites@gmail.com>
2024-12-20 23:36:08 +0530

    creating react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+   <h1>Absolute Cinema</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return(
+     <h1>Vite is nice</h1>
+  )
+}
+
+export default App
