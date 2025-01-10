1070c3c020a7f842637dda2c13d5c118d29e48e9
Tanmay Chaturvedi <tanmayreadsandwrites@gmail.com>
2025-01-10 20:19:16 +0530

    theme switcher (more context api)

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, {useEffect, useState} from 'react'
+import { ThemeProvider } from './contexts/Theme'
+import Button from './components/Button'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const lightTheme = () => {
+    return setThemeMode("light")
+  }
+  const darkTheme = () => {
+    return setThemeMode("dark")
+  }
+
+  //actual change in theme
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+    return (
+      <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+        <>
+          <h1 className='p-4 bg-blue-700 text-3xl text-white'>React</h1>
+          <div className="flex flex-wrap min-h-screen items-center">
+              <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <Button />
+                </div>
+                <div className="w-full max-w-sm mx-auto">
+                  <Card />
+                </div>
+              </div>
+          </div>
+        </>
+      </ThemeProvider>
+    )
+  
 }
 
 export default App
